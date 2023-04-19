f2f0ebdd988de2f7c54086385c43d549ba0909ee
DS <mailforgautamvg@gmail.com>
2023-04-19 15:52:04 +0530

    changed sqlite database filename

--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize('sqlite:memory:')
+const sequelize = new Sequelize('sqlite:db.db')
 
 export async function initDB() {
 	await sequelize.sync({ force: true })
