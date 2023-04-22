de61a8352a0dfb55d2535f7490463e42b7c8cfff
DS <mailforgautamvg@gmail.com>
2023-04-22 14:11:36 +0530

    disabled fcm msg sending

--- src/controllers/hypertrack_webhook.ts
+++ src/controllers/hypertrack_webhook.ts
@@ -7,11 +7,12 @@ import logger from '../lib/logger'
 export const controller: RequestHandler = async (req, res, next) => {
 	res.sendStatus(200)
 
-	console.log('Webhook: ')
-	console.log(JSON.stringify(req.body))
+	if (Array.isArray(req.body)) req.body.forEach(evaluate)
+	else evaluate(req.body)
 
-	if (req.body.hasOwnProperty('device_id')) {
-		if ((req.body['type'] = 'device_status'))
+	async function evaluate(data: any) {
+		logger.info('Reading webhook data: ' + JSON.stringify(data))
+		if (data.hasOwnProperty('device_id')) {
 			try {
 				const appointment = await Appointment.findOne({
 					where: {
@@ -19,19 +20,20 @@ export const controller: RequestHandler = async (req, res, next) => {
 					},
 				})
 
-				const success = await fcm_send_msg(
-					req.body,
-					appointment?.getDataValue('fcm_registration_token')
-				)
-
-				if (success) logger.info('Sent message in response to webhook')
+				if (appointment == null)
+					logger.error('Received unknown device id in webhook')
+				// const success = await fcm_send_msg(
+				// 	data,
+				// 	appointment!.getDataValue('fcm_registration_token')
+				// )
+				// if (success) logger.info('Sent message in response to webhook')
 				else
 					logger.info('Could not send message in response to webhook')
 			} catch (e) {
-				logger.error('Received unknown device id in webhook')
+				logger.error('Error in webhook: ' + JSON.stringify(e))
 			}
-	} else {
-		logger.info('Received no device id in webhook')
-		console.log('NO')
+		} else {
+			logger.info('Received no device id in webhook')
+		}
 	}
 }
